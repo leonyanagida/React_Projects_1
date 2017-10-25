c8af4a07836a071e2f3538fa6eafe064efa7027b
littlejohngil <Johnyanagida@yahoo.com>
2017-10-24 23:03:43 -0700

    Updated the github link

--- react_githubs/src/components/footer.js
+++ react_githubs/src/components/footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <a href="https://github.com/littlejohngil" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-2x"></i></a>
+        <a href="https://github.com/leonyanagida" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-2x"></i></a>
       </footer>
     );
   }
